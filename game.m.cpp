e3cc50e28358a777a0109b9172a1d20e4b23d626
Shaokang Wang <swang927@gmail.com>
2015-09-07 17:53:30 -0500

    Add game.m.cpp which contain the main funtion

---
+++ game.m.cpp
@@ -0,0 +1,14 @@
+// game.m.cpp
+/* 
+	Author: Shaokang Wang
+	Namespace: StevensDev
+*/
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello CS585, I am ready to fight\n";
+
+    return 0;
+}
