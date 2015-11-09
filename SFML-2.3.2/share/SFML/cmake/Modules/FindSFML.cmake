e650ae771a2372114d0b7dba2a543c382765fa8a
Shaokang Wang <swang927@gmail.com>
2015-11-09 14:05:51 -0500

    Fix the cmake file for linking sfml

--- SFML-2.3.2/share/SFML/cmake/Modules/FindSFML.cmake
+++ SFML-2.3.2/share/SFML/cmake/Modules/FindSFML.cmake
@@ -100,8 +100,10 @@ if(SFML_FIND_VERSION AND SFML_INCLUDE_DIR)
     # if we could extract them, compare with the requested version number
     if (SFML_VERSION_MAJOR)
         # transform version numbers to an integer
-		math(EXPR SFML_VERSION "SFML_VERSION_MAJOR * 10000 + SFML_VERSION_MINOR * 100")
-
+		message(STATUS ${SFML_VERSION_MAJOR})
+		message(STATUS ${SFML_VERSION_MINOR})
+		#math(EXPR SFML_VERSION "${SFML_VERSION_MAJOR} * 10000 + ${SFML_VERSION_MINOR} * 100")
+		message(STATUS ${SFML_VERSION})
         # compare them
         if(SFML_VERSION LESS SFML_REQUESTED_VERSION)
             set(SFML_VERSION_OK FALSE)
